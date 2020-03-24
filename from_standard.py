1a59a4c8707010e05ecc2cfffee92ea7bb8ff063
Alice Lo Valvo <alicelovalvo@yahoo.it>
2020-03-24 22:41:55 +0100

    add intensive care

--- from_standard.py
+++ from_standard.py
@@ -15,6 +15,7 @@ def main():
     df_fatalities = pd.DataFrame()
     df_recovered = pd.DataFrame()
     df_hospital = pd.DataFrame()
+    df_intensive_care = pd.DataFrame()
         
     regioni = df_regione["denominazione_regione"].unique()
     
@@ -26,24 +27,29 @@ def main():
                 df_fatalities.loc[dt,reg] = row["deceduti"]
                 df_recovered.loc[dt,reg] = row["dimessi_guariti"]
                 df_hospital.loc[dt,reg] = row["totale_ospedalizzati"]
+                df_intensive_care.loc[dt,reg] = row["terapia_intensiva"]
         
         
     df_cases["Italia"] = df_cases.sum(axis=1)
     df_fatalities["Italia"] = df_fatalities.sum(axis=1)
     df_recovered["Italia"] = df_recovered.sum(axis=1)
     df_hospital["Italia"] = df_hospital.sum(axis=1)
+    df_intensive_care["Italia"] = df_intensive_care.sum(axis=1)
     df_cases.index.name = "Date"
     df_fatalities.index.name = "Date"
     df_recovered.index.name = "Date"
     df_hospital.index.name = "Date"
+    df_intensive_care.index.name = "Date"
     df_cases = df_cases.astype(int)
     df_fatalities = df_fatalities.astype(int)
     df_recovered = df_recovered.astype(int)
     df_hospital = df_hospital.astype(int)
+    df_intensive_care = df_intensive_care.astype(int)
     df_cases.to_csv("covid19_cases_italy.csv", index=True)
     df_fatalities.to_csv("covid19_fatalities_italy.csv", index=True)
     df_recovered.to_csv("covid19_recovered_italy.csv", index=True)
     df_hospital.to_csv("covid19_hospital_italy.csv", index=True)
+    df_intensive_care.to_csv("covid19_intensive_care_italy.csv", index=True)
 
 
 if __name__ == "__main__":
