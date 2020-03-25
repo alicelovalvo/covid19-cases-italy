3ec8d4ccbd1ff70c673df59e207ea6e9cc9d8928
Alice Lo Valvo <alicelovalvo@yahoo.it>
2020-03-25 19:17:33 +0100

    add data

--- from_standard.py
+++ from_standard.py
@@ -16,6 +16,7 @@ def main():
     df_recovered = pd.DataFrame()
     df_hospital = pd.DataFrame()
     df_intensive_care = pd.DataFrame()
+    df_positive_cases = pd.DataFrame()
         
     regioni = df_regione["denominazione_regione"].unique()
     
@@ -28,6 +29,7 @@ def main():
                 df_recovered.loc[dt,reg] = row["dimessi_guariti"]
                 df_hospital.loc[dt,reg] = row["totale_ospedalizzati"]
                 df_intensive_care.loc[dt,reg] = row["terapia_intensiva"]
+                df_positive_cases.loc[dt,reg] = row["totale_attualmente_positivi"]
         
         
     df_cases["Italia"] = df_cases.sum(axis=1)
@@ -36,22 +38,25 @@ def main():
     df_hospital["Italia"] = df_hospital.sum(axis=1)
     df_intensive_care["Posti Occupati"] = df_intensive_care.sum(axis=1)
     df_intensive_care["Posti Disponibili"] = '5324'
+    df_positive_cases["Italia"] = df_positive_cases.sum(axis=1)
     df_cases.index.name = "Date"
     df_fatalities.index.name = "Date"
     df_recovered.index.name = "Date"
     df_hospital.index.name = "Date"
     df_intensive_care.index.name = "Date"
+    df_positive_cases.index.name = "Date"
     df_cases = df_cases.astype(int)
     df_fatalities = df_fatalities.astype(int)
     df_recovered = df_recovered.astype(int)
     df_hospital = df_hospital.astype(int)
+    df_positive_cases = df_positive_cases.astype(int)
     df_intensive_care = df_intensive_care.astype(int)
     df_cases.to_csv("covid19_cases_italy.csv", index=True)
     df_fatalities.to_csv("covid19_fatalities_italy.csv", index=True)
     df_recovered.to_csv("covid19_recovered_italy.csv", index=True)
     df_hospital.to_csv("covid19_hospital_italy.csv", index=True)
     df_intensive_care.to_csv("covid19_intensive_care_italy.csv", index=True)
-
+    df_positive_cases.to_csv("covid19_positive_cases_italy.csv", index=True)
 
 if __name__ == "__main__":
     main()
