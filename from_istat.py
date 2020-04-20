be0651fb4f58b049917e486beb8613551f4d991a
Alice Lo Valvo <alicelovalvo@yahoo.it>
2020-04-20 18:27:02 +0200

    new data

---
+++ from_istat.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 13:17:36 2020
+
+@author: Alice
+"""
+
+import pandas as pd
+from datetime import datetime
+
+
+def main():
+    """ Transforms the data from the standards csv to my csv """
+
+    df = pd.read_excel('Tavola_sintetica_ISTAT.xlsx', sheet_name='Totale per sesso')
+    
+    df_regione = pd.read_csv('regioni.csv', error_bad_lines=False)
+
+
+    # df_cases = pd.DataFrame()
+    # df_fatalities = pd.DataFrame()
+    # df_recovered = pd.DataFrame()
+    # df_hospital = pd.DataFrame()
+    # df_intensive_care = pd.DataFrame()
+    # df_positive_cases = pd.DataFrame()
+    df_fatalities_istat = pd.DataFrame()
+        
+    regioni = df_regione["Region"].unique()
+    
+    for i, row in df.iterrows():    
+    #     dt = datetime.fromisoformat(row["data"]).date()
+        for reg in regioni:
+            if row["Unnamed: 2"] == reg:
+                # print(row)
+                df_fatalities_istat.loc['2019', reg]= 0
+                df_fatalities_istat.loc['2020', reg]= 0
+    
+    
+    for i, row in df.iterrows():    
+        for reg in regioni:
+            if row["Unnamed: 2"] == reg:
+                df_fatalities_istat.loc['2019', reg]+= row["Unnamed: 9"]
+                df_fatalities_istat.loc['2020', reg]+= row["Unnamed: 12"]
+
+        
+    df_fatalities_istat["Italia"] = df_fatalities_istat.sum(axis=1)
+
+    df_fatalities_istat.index.name = "Date"
+    df_fatalities_istat = df_fatalities_istat.astype(int)
+
+    df_fatalities_istat.to_csv("df_fatalities_istat.csv", index=True)
+
+if __name__ == "__main__":
+    main()
