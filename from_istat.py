bff618aef96675e463ec7ca245d80e2e16454b67
Alice Lo Valvo <alicelovalvo@yahoo.it>
2020-04-21 01:25:01 +0200

    new data

--- from_istat.py
+++ from_istat.py
@@ -25,6 +25,7 @@ def main():
     # df_intensive_care = pd.DataFrame()
     # df_positive_cases = pd.DataFrame()
     df_fatalities_istat = pd.DataFrame()
+    df_percentage = pd.DataFrame()
     dt = [2019, 2020]
     regioni = df_regione["Region"].unique()
     
@@ -43,14 +44,17 @@ def main():
                 df_fatalities_istat.loc[dt[0], reg]+= row["Unnamed: 9"]
                 df_fatalities_istat.loc[dt[1], reg]+= row["Unnamed: 12"]
 
-        
+    for reg in regioni:
+        df_percentage.loc[1,reg] = (df_fatalities_istat.loc[dt[1], reg] - df_fatalities_istat.loc[dt[0], reg])/df_fatalities_istat.loc[dt[0], reg]*100
+    
+    
     df_fatalities_istat["Italia"] = df_fatalities_istat.sum(axis=1)
-
     df_fatalities_istat.index.name = "Date"
     df_fatalities_istat = df_fatalities_istat.astype(int)
-    print(df_fatalities_istat)
 
+    df_percentage.index.name = "Date"
     df_fatalities_istat.to_csv("df_fatalities_istat.csv", index=True)
+    df_percentage.to_csv("df_percentage.csv", index=True)
 
 if __name__ == "__main__":
     main()
