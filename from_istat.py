f9b09f8e4306319109da12ad192c802a2a025b46
Alice Lo Valvo <alicelovalvo@yahoo.it>
2020-04-25 20:44:16 +0200

    new data

--- from_istat.py
+++ from_istat.py
@@ -7,7 +7,6 @@ Created on Mon Apr 20 13:17:36 2020
 """
 
 import pandas as pd
-from datetime import datetime
 
 
 def main():
@@ -25,24 +24,15 @@ def main():
     df_filter = pd.DataFrame()
     df_filter = df_fatalities_years[df_fatalities_years.COMUNE.isin(list_codes)]
     
-    print(df_filter)
-
-    # df_cases = pd.DataFrame()
-    # df_fatalities = pd.DataFrame()
-    # df_recovered = pd.DataFrame()
-    # df_hospital = pd.DataFrame()
-    # df_intensive_care = pd.DataFrame()
-    # df_positive_cases = pd.DataFrame()
+    
     df_fatalities_istat = pd.DataFrame()
     df_percentage = pd.DataFrame()
     dt = [2015, 2016, 2017, 2018, 2019, 2020]
     regioni = df_regione["Region"].unique()
     
     for i, row in df.iterrows():    
-    #     dt = datetime.fromisoformat(row["data"]).date()
         for reg in regioni:
             if row["Unnamed: 2"] == reg:
-                # print(row)
                 df_fatalities_istat.loc[dt[0], reg]= 0
                 df_fatalities_istat.loc[dt[1], reg]= 0
                 df_fatalities_istat.loc[dt[2], reg]= 0
@@ -52,11 +42,12 @@ def main():
     
     for i, row in df_filter.iterrows():
         for reg in regioni:
-            if row["REGIONI"] == reg:
-                df_fatalities_istat.loc[dt[0], reg]+= row["DECESSI_2015"]
-                df_fatalities_istat.loc[dt[1], reg]+= row["DECESSI_2016"]
-                df_fatalities_istat.loc[dt[2], reg]+= row["DECESSI_2017"]
-                df_fatalities_istat.loc[dt[3], reg]+= row["DECESSI_2018"]
+            if row["NOME_REGIONE"] == reg:
+                if (row["MESE_DECESSO"] == 3) or (row["MESE_DECESSO"] == 4 and row["GIORNO_DECESSO"] < 5):
+                    df_fatalities_istat.loc[dt[0], reg]+= row["DECESSI_2015"]
+                    df_fatalities_istat.loc[dt[1], reg]+= row["DECESSI_2016"]
+                    df_fatalities_istat.loc[dt[2], reg]+= row["DECESSI_2017"]
+                    df_fatalities_istat.loc[dt[3], reg]+= row["DECESSI_2018"]
     
     for i, row in df.iterrows():    
         for reg in regioni:
@@ -65,7 +56,7 @@ def main():
                 df_fatalities_istat.loc[dt[5], reg]+= row["Unnamed: 12"]
 
     for reg in regioni:
-        df_percentage.loc[1,reg] = (df_fatalities_istat.loc[dt[1], reg] - df_fatalities_istat.loc[dt[0], reg])/df_fatalities_istat.loc[dt[0], reg]*100
+        df_percentage.loc[1,reg] = (df_fatalities_istat.loc[dt[5], reg] - df_fatalities_istat.loc[dt[4], reg])/df_fatalities_istat.loc[dt[4], reg]*100
     
     
     
